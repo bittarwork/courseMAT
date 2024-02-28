086dc821d1660e6a40810008952a7a649d9c68e7
bittarwork <81069087+bittarwork@users.noreply.github.com>
2024-02-28 15:14:14 +0300

    adding the res fiels

---
+++ الجلسة الثانية/فقرة جانبية/session 2 question solving for some problem/session 2 question solving for some problem/session 2 question solving for some problem.cpp
@@ -0,0 +1,72 @@
+// session 2 question solving for some problem.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+
+#include <iostream>
+
+// first problem:
+//int absoluteValue(int x) {
+//    if (x < 0) {
+//        return -x;
+//    }
+//    else {
+//        return x;
+//    }
+//}
+//
+//int main() {
+//    int x;
+//    std::cout << "Enter an integer: ";
+//    std::cin >> x;
+//
+//    int absolute = absoluteValue(x);
+//    std::cout << "The absolute value of " << x << " is " << absolute << std::endl;
+//
+//    return 0;
+//}
+///////////////////////////////////////////////////////////////////
+// 
+// seconde problem: 
+//const double PI = 3.1415927;
+//
+//int main() {
+//    double diameter;
+//    std::cout << "Enter the diameter of the circle: ";
+//    std::cin >> diameter;
+//
+//    double radius = diameter / 2;
+//    double circumference = 2 * PI * radius;
+//    double area = PI * radius * radius;
+//
+//    std::cout << "Diameter: " << diameter << std::endl;
+//    std::cout << "Radius: " << radius << std::endl;
+//    std::cout << "Circumference: " << circumference << std::endl;
+//    std::cout << "Area: " << area << std::endl;
+//
+//    return 0;
+//}
+
+///////////////////////////////////////////////////////////////////
+
+// third problem:
+//
+//double calculateWaveVelocity(double frequency, double wavelength) {
+//    if (frequency == 0 || wavelength == 0) {
+//        std::cout << "Frequency and wavelength cannot be zero." << std::endl;
+//        return 0;
+//    }
+//    double velocity = frequency * wavelength;
+//    return velocity;
+//}
+//
+//int main() {
+//    double frequency, wavelength;
+//    std::cout << "Enter the frequency of the wave (in Hz): ";
+//    std::cin >> frequency;
+//    std::cout << "Enter the wavelength of the wave (in meters): ";
+//    std::cin >> wavelength;
+//
+//    double waveVelocity = calculateWaveVelocity(frequency, wavelength);
+//    std::cout << "The wave velocity is: " << waveVelocity << " m/s" << std::endl;
+//
+//    return 0;
+//}
